b0499ac2d7421313e35407467abe6f7c2e4735c0
NickMiras2024 <162518340+NickMiras2024@users.noreply.github.com>
2024-04-22 21:13:59 -0300

    Update añadirUsuario4.jsx

--- src/components/añadirUsuario4.jsx
+++ src/components/añadirUsuario4.jsx
@@ -4,17 +4,6 @@ import { useContext } from 'react';
 
 
 
-function ItemUser4({ data,url }) {
-
-    const {ruta}= useContext(contextoGood)
-
-    let nombreArch;
-import '../styles/itemUser.css'
-import { contextoGood } from '../App';
-import { useContext } from 'react';
-
-
-
 function ItemUser4({ data, url }) {
 
     const { ruta } = useContext(contextoGood)
@@ -50,25 +39,6 @@ function ItemUser4({ data, url }) {
                 ></div>
                 <iframe src={nombreArch} className='fra' style={{ background: "#fff" }}></iframe>
             </td>
-
-        </tr>
-    );
-}
-
-export default ItemUser4;
-    if(url){
-    nombreArch = `https://docs.google.com/gview?url=${ruta}/documentos/:${url}&embedded=true`
-
-    }
-
-    console.log(data,url,nombreArch)
-    return (
-        <tr>
-            <td> {data.dia} </td>
-            <td> {data.hs} </td>
-            <td> {data.entrenamientos} </td>
-            <td> <iframe src={nombreArch} style={{background:"#fff"}} frameBorder="0"></iframe></td>
-
 
         </tr>
     );
