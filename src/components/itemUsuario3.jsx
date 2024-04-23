4627b155558f6aede37689cc44436d045f218867
NickMiras2024 <162518340+NickMiras2024@users.noreply.github.com>
2024-04-22 21:13:39 -0300

    Update itemUsuario3.jsx

--- src/components/itemUsuario3.jsx
+++ src/components/itemUsuario3.jsx
@@ -5,19 +5,6 @@ import { useContext } from 'react';
 
 
 
-function ItemUser3({ data }) {
-
-    const {cargarEntrenamiento,ruta}= useContext(contextoGood)
-
-
-
-    function eliminar2(data2) {import '../styles/itemUser.css'
-import tachito from '../assets/basura.png'
-import { contextoGood } from '../App';
-import { useContext } from 'react';
-
-
-
 function ItemUser3({ data,url }) {
 
     const {cargarEntrenamiento,ruta}= useContext(contextoGood)
@@ -85,43 +72,6 @@ function ItemUser3({ data,url }) {
                 />
             </td>
             
-
-        </tr>
-    );
-}
-
-export default ItemUser3;
-        const confirm = window.confirm('seguro que quieres eliminar este entrenamiento')
-        if (confirm) {
-            fetch(`${ruta}/eliminarEntrenamiento`, {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data2)
-            });
-        cargarEntrenamiento()
-        // {"userDNI":data.UsuarioDNI}
-        } else {
-            // console.log(data2)
-        }
-
-    }
-
-
-
-
-    console.log(data)
-    return (
-        <tr className="I">
-            <td> {data.dia} </td>
-            <td> {data.hs} </td>
-            <td> {data.entrenamientos} </td>
-            <td>
-                <img className="botton" src={tachito}
-                    onClick={() => { eliminar2({"id": data.id})}}
-                />
-            </td>
 
         </tr>
     );
